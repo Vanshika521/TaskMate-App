7593f72d3bce1737f8057dba933b1a1880b53f66
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-08-20 13:02:24 +0530

    add new notes

--- app/src/main/java/com/codsoft/todolistapp/MainActivity.java
+++ app/src/main/java/com/codsoft/todolistapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.codsoft.todolistapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+       // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
+        fBtn = findViewById(R.id.add_notes);
+
+
+        fBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this,Notes_Details.class);
+                startActivity(intent);
+            }
         });
-        //
+
+
+
+
     }
 }
